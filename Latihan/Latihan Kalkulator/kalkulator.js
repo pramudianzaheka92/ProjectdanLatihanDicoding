671e674a0de992a7a70528b8f9562448507d7e5b
Eka Pramudianzah <pramudianzah334@gmail.com>
2022-07-19 11:32:56 +0700

    Menambahkan File JavaScript di Latihan Kalkulator Website

---
+++ Latihan/Latihan Kalkulator/kalkulator.js
@@ -0,0 +1,28 @@
+// Script ini akan berisi script dari bahasa pemrograman JavaScript
+
+
+/*Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus tortor dolor, dapibus a interdum pretium, 
+rhoncus a dolor. Curabitur ligula massa, posuere quis eros et, dictum iaculis arcu. Nulla bibendum quam a quam volutpat, 
+sed ullamcorper enim mollis. Maecenas placerat finibus dictum. Vestibulum sollicitudin est nunc, 
+quis varius metus condimentum volutpat. In eget sodales leo, at tincidunt nisi. In at arcu fermentum, 
+efficitur ipsum sed, posuere purus. Suspendisse fermentum odio vel ex aliquet, eu vehicula turpis dignissim. 
+Phasellus fermentum auctor urna id efficitur. Interdum et malesuada fames ac ante ipsum primis in faucibus. 
+Curabitur eros libero, posuere id ornare in, lobortis id nunc. Suspendisse potenti. Praesent est urna, 
+tempor vel lectus id, faucibus gravida mauris. Aliquam ut mi vitae enim iaculis rutrum quis eget nibh.
+*/
+
+let user = {
+    namaLengkap: "Eka Pramudianzah",
+    namaPanggilan: "Eka", 
+    umur: 22, 
+    tinggiBadan: 172.5,
+    stuff: ["Magic Wind", "Flying Car", "Owl"]
+};
+
+console.log("Hallo, nama saya " + user.namaLengkap + " dan Kalian bisa panggil saya " + user.namaPanggilan);
+console.log("Umur saya " + user.umur + " tahun" + " dan Tinggi Badan saya " + user.tinggiBadan + " cm.");
+
+/* output
+Hallo, nama saya Harry Potter
+Umur saya 20 tahun
+*/
