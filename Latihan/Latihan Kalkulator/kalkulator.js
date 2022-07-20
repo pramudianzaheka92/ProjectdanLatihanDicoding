ea5da80b7ef8cee4b0f6b0c5c09b4cca4ce0b50f
Eka Pramudianzah <pramudianzah334@gmail.com>
2022-07-20 10:49:24 +0700

    Menambahkan File Latihan Web Storage dan Folder Submission

--- Latihan/Latihan Kalkulator/kalkulator.js
+++ Latihan/Latihan Kalkulator/kalkulator.js
@@ -64,3 +64,37 @@ function showAngka() {
    x = 9; // ini akan dianggap error karena variabel belum dideklarasikan
   alert(x);
 }*/
+
+const calculator = {
+    displayNumber: '0',
+    operator: null,
+    firstNumber: null,
+    isWaitForSecondNumber: false,
+};
+
+function updateDisplay() {
+    document.querySelector('#displayNumber').innerText = calculator.displayNumber;
+}
+   
+function clearCalculator() {
+    calculator.displayNumber = '0';
+    calculator.operator = null;
+    calculator.firstNumber = null;
+    calculator.isWaitForSecondNumber = false;
+}
+
+function inputDigit(digit) {
+    calculator.displayNumber += digit;
+}
+
+const buttons = document.querySelectorAll('.button');
+
+for (const button of buttons) {
+    button.addEventListener('click', function (event) {
+    // mendapatkan objek elemen yang diklik
+    const target = event.target;
+ 
+    inputDigit(target.innerText);
+    updateDisplay();
+  });
+}
