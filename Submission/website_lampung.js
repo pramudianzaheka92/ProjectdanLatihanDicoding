a23b7a9061a6b5b164a3c8bad30394e7e4329024
Eka Pramudianzah <pramudianzah334@gmail.com>
2022-07-21 11:40:48 +0700

    Menyelesaikan Bug yang ada di dalam Navigation Menu di Web Submission

--- Submission/website_lampung.js
+++ Submission/website_lampung.js
@@ -1,35 +1,43 @@
-/* When the user clicks on the button, toggle between hiding and showing the dropdown content */
+/* Membuat fungsi untuk menampung menu dropdown yang ada di html, tepatnya fungsi ini dibuat untuk menu pariwisata
+karena di menu tersebut terdapat sub-menu seperti konten tentang pantai pasir putih, way kambas, dan pulau pahawang */
 function FunctionPariwisata() {
     document.getElementById("DropdownPariwisata").classList.toggle("show-pariwisata");
 }
 
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(event) {
-    if (!event.target.matches(".dropbtnpariwisata")) {
-        var dropdowns = document.getElementsByClassName("dropdown-pariwisata");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains("show-pariwisata")) {
-                openDropdown.classList.remove("show-pariwisata");
-            }
-        }
-    }
-}
-
+/* Membuat fungsi untuk menampung menu dropdown yang ada di html, tepatnya fungsi ini dibuat untuk menu budaya
+karena di menu tersebut terdapat sub-menu seperti konten tentang tapis lampung dan seni tari sigeh pengunten */
 function FunctionBudaya() {
     document.getElementById("DropdownBudaya").classList.toggle("show-budaya");
 }
 
-// Close the dropdown if the user clicks outside of it
+
+/* Event untuk kedua menu dropdown yaitu menu pariwisata dan budaya. 
+Jadi jika user meng-klik salah satu dari sub menu atau meng-klik 
+diluar dropdown maka dropdown akan tertutup secara otomatis */
 window.onclick = function(event) {
+
+    /* Event untuk button menu pariwisata yang didalamnya terdapat konten tentang pasir putih, 
+    way kambas, dan pulau pahawang. Jadi jika salah satu sub-menu diklik maka dropdown akan menutup */
+    if (!event.target.matches(".dropbtnpariwisata")) {
+        let dropdownspariwisata = document.getElementsByClassName("dropdown-pariwisata");
+        let i;
+        for (i = 0; i < dropdownspariwisata.length; i++) {
+            let openDropdownPariwisata = dropdownspariwisata[i];
+            if (openDropdownPariwisata.classList.contains("show-pariwisata")) {
+                openDropdownPariwisata.classList.remove("show-pariwisata");
+            }
+        }
+    }
+
+    /* Event untuk button menu budaya yang didalamnya terdapat konten tentang tapis lampung, 
+    dan seni tari sigeh pengunten. Jadi jika salah satu sub-menu diklik maka dropdown akan menutup */
     if (!event.target.matches(".dropbtnbudaya")) {
-        var dropdowns = document.getElementsByClassName("dropdown-budaya");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains("show-budaya")) {
-                openDropdown.classList.remove("show-budaya");
+        let dropdownsbudaya = document.getElementsByClassName("dropdown-budaya");
+        let i;
+        for (i = 0; i < dropdownsbudaya.length; i++) {
+            let openDropdownBudaya = dropdownsbudaya[i];
+            if (openDropdownBudaya.classList.contains("show-budaya")) {
+                openDropdownBudaya.classList.remove("show-budaya");
             }
         }
     }
